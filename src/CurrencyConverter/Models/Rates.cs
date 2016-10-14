c6e2a13229d4613a8cd426e0b10eccaa4eae0404
Malwine <malwine@mailbox.org>
2016-10-14 16:25:24 +0100

    start xml parsing

--- src/CurrencyConverter/Models/Rates.cs
+++ src/CurrencyConverter/Models/Rates.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace CurrencyConverter.Models
 {
     public static class Rates
     {
+        public static string Parsing()
+        {
+            string text = "";
+            XmlDocument doc = new XmlDocument();
+            doc.Load("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+            foreach (XmlNode node in doc.DocumentElement.SelectSingleNode("currency"))
+            {
+                text = node.InnerText;
+            }
+            return text;
+        }
 
         public static Dictionary<string, int> AllRates() {
-           return new Dictionary<string, int>() { { "EUR", 1 }, { "USD", 2}, {"JPY", 40} };
+            return new Dictionary<string, int>() { { "EUR", 1 }, { "USD", 2}, {"JPY", 40} };
         }
 
         public static int FindRateFor(string currency)
